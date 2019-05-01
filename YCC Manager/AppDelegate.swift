02691121896fb8d71de3268128d4f8f2a5ed046a
Dr Vikram Raj Gopinathan <dr.vikramraj87@gmail.com>
2019-05-01 14:13:04 +0530

    Initial Commit

---
+++ YCC Manager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  YCC Manager
+//
+//  Created by Vikram Raj Gopinathan on 01/05/19.
+//  Copyright © 2019 Vikram Raj Gopinathan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ YCC Manager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YCC Manager
+//
+//  Created by Vikram Raj Gopinathan on 01/05/19.
+//  Copyright © 2019 Vikram Raj Gopinathan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
